ab736e857502b4dc6575eb4ba8b10a1feee32ede
Matteo <passematteo@gmail.com>
2023-07-20 17:54:17 +0200

    Add getRecipe method on main file
    - getRecipe method -> get data of the recipe webpage

--- src/RecipeScrapers.php
+++ src/RecipeScrapers.php
@@ -2,13 +2,32 @@
 
 namespace RecipeScrapers;
 
+use RecipeScrapers\models\MarmitonRecipe;
 use RecipeScrapers\models\Recipe;
 use RecipeScrapers\utils\Type;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
 
 class RecipeScrapers
 {
-
     public \ErrorException $error;
+
+    public function getRecipe(String $uri) : ?Recipe
+    {
+        if($this->checkUri($uri)){
+            $client = new HttpBrowser(HttpClient::create());
+            $crawler = $client->request("GET", $uri);
+            switch ($this->getSource($uri)){
+                case Type::MARMITON:
+                    $data = $crawler->filterXPath('//body/script')->text();
+                    return new MarmitonRecipe($data); // Todo 
+                default:
+                    $this->error = new \ErrorException("This website isn't available");
+                    break;
+            }
+        }
+        return null;
+    }
 
     public function checkUri(String $uri) : bool
     {
