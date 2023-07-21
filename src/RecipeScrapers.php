8a17288e6c6e214506176b360d74c164d24f6aee
Matteo <passematteo@gmail.com>
2023-07-21 17:30:34 +0200

    add method on main class
    - Add isValidSchema to check if the data is a valid json and a shema.org/Recipe model

--- src/RecipeScrapers.php
+++ src/RecipeScrapers.php
@@ -2,6 +2,7 @@
 
 namespace RecipeScrapers;
 
+use ErrorException;
 use RecipeScrapers\models\MarmitonRecipe;
 use RecipeScrapers\models\Recipe;
 use RecipeScrapers\utils\Type;
@@ -10,7 +11,7 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class RecipeScrapers
 {
-    public \ErrorException $error;
+    public ErrorException $error;
 
     public function getRecipe(String $uri) : ?Recipe
     {
@@ -19,10 +20,14 @@ class RecipeScrapers
             $crawler = $client->request("GET", $uri);
             switch ($this->getSource($uri)){
                 case Type::MARMITON:
-                    $data = $crawler->filterXPath('//body/script')->text();
-                    return new MarmitonRecipe($data); // Todo 
+                    $data = $crawler->filterXPath('//head/script')->text();
+                    $json = json_decode($data, true);
+                    if ($this->isValidSchema($json)){
+                        return new MarmitonRecipe($json, $data);
+                    }else $this->error = new ErrorException("Invalid data");
+                    break;
                 default:
-                    $this->error = new \ErrorException("This website isn't available");
+                    $this->error = new ErrorException("This website isn't available");
                     break;
             }
         }
@@ -33,7 +38,7 @@ class RecipeScrapers
     {
         $url = parse_url($uri);
         if(!is_array($url)){
-            $this->error = new \ErrorException("Invalid uri");
+            $this->error = new ErrorException("Invalid uri");
             return false;
         }
         return true;
@@ -42,5 +47,15 @@ class RecipeScrapers
     public function getSource(String $uri): String
     {
         return parse_url($uri, PHP_URL_HOST);
+    }
+
+    private function isValidSchema(mixed $json): bool
+    {
+        if (!is_null($json)){
+            if ($json["@context"] === "https://schema.org" and $json["@type"] === "Recipe"){
+                return true;
+            }
+        }
+        return false;
     }
 }
